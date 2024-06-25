3ea84d338720692c1d15d2ad1ce5ef6be8b9f19e
bmm333 <81256935+bmm333@users.noreply.github.com>
2024-06-25 22:43:39 +0200

    p
    kot

---
+++ k/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
+#include <stdbool.h>
+
+int main(int argc,char* argv[])
+{
+	if(argc!=2)
+	{
+		fprintf("Usage %s:<filename>",argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	/*Still to work*/
+}
